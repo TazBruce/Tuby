09b56a92b707cb252f0101acbc6f49b21c467b4f
Taz <54491493+tazbruce@users.noreply.github.com>
2020-07-02 22:00:41 +1200

    Moved progress bar to main GUI Sizing changes Disabled theme Success popup

--- Tuby.py
+++ Tuby.py
@@ -1,24 +1,24 @@
-from pytube import YouTube
 import PySimpleGUI as sg
+from pytube import YouTube
 
 file_size = 0
 
 
 def progress_check(stream, chunk, remaining):
     percent = (100 * (file_size - remaining)) / file_size
-    sg.one_line_progress_meter('Progress Bar', current_value=percent, max_value=100, key="_BAR_",
-                               orientation="horizontal")
+    window["_BAR_"].UpdateBar(percent)
 
 
-sg.ChangeLookAndFeel('DarkRed1')
+# sg.ChangeLookAndFeel('DarkRed1')
 # sg.SetGlobalIcon('logo.ico')
 
 layout = [[sg.T("Tuby")],
           [sg.T("URL")],
-          [sg.I(key="_URL_")],
+          [sg.I(key="_URL_", size=(42, 18))],
           [sg.T("Destination")],
-          [sg.I(" ", key="_FOLDER_"), sg.FolderBrowse()],
-          [sg.B("Download")]
+          [sg.I(" ", key="_FOLDER_", size=(42, 18)), sg.FolderBrowse()],
+          [sg.B("Download")],
+          [sg.ProgressBar(max_value=100, orientation="horizontal", key="_BAR_", size=(36, 25))]
           ]
 window = sg.Window('Tuby', layout, default_element_size=(50, 1), resizable=False, font="Helvetica").finalize()
 while True:
@@ -34,5 +34,7 @@ while True:
         print(title)
         file_size = video_type.filesize
         video_type.download(values["_FOLDER_"], filename=title)
+        window["_BAR_"].UpdateBar(0)
+        sg.popup("Complete!", auto_close=False)
 
 window.close()
