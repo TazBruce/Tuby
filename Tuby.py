a3e53b0a9763dd85bdc67e13a9fb88d78a76b988
Taz <tasmanbrucework@gmail.com>
2020-07-02 21:09:40 +1200

    Fixed functionality Began working on titles

--- Tuby.py
+++ Tuby.py
@@ -4,8 +4,8 @@ import PySimpleGUI as sg
 file_size = 0
 
 
-def progress_check(stream=None, chunk=None, file_handle=None, remaining=None):
-    percent = (100*(file_size-remaining))/file_size
+def progress_check(stream, chunk, remaining):
+    percent = (100 * (file_size - remaining)) / file_size
     sg.one_line_progress_meter('Progress Bar', current_value=percent, max_value=100, key="_BAR_",
                                orientation="horizontal")
 
@@ -18,7 +18,7 @@ layout = [[sg.T("Tuby")],
           [sg.I(key="_URL_")],
           [sg.T("Destination")],
           [sg.I(" ", key="_FOLDER_"), sg.FolderBrowse()],
-          [sg.B("Download")],
+          [sg.B("Download")]
           ]
 window = sg.Window('Tuby', layout, default_element_size=(50, 1), resizable=False, font="Helvetica").finalize()
 while True:
@@ -27,12 +27,12 @@ while True:
     if event in (None, 'Exit'):
         break
     elif event == "Download":
-        sg.popup("Starting Download...", auto_close=True, auto_close_duration=3)
+        sg.popup("Starting Download...", auto_close=True, auto_close_duration=1)
         video = YouTube(values["_URL_"], on_progress_callback=progress_check)
         video_type = video.streams.filter(progressive=True, file_extension="mp4").first()
         title = video.title
+        print(title)
         file_size = video_type.filesize
-        video_type.download(values["_FOLDER_"])
-
+        video_type.download(values["_FOLDER_"], filename=title)
 
 window.close()
